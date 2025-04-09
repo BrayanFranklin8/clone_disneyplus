26805ac97c60ba506c09fcead5c98d2bbe16c0be
BRAYAN <brayanfranklin88@gmail.com>
2025-04-09 19:01:18 -0300

    bem atualizado

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        console.log(window.scrollY);
+    })
+
+
+    // seção de atraçoes,progamação das abas
     
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click',function(botao){
@@ -11,7 +22,21 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    //seção FAQ, acorddion
+
+
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click',abreOuFechaResposta)
+    }
 })
+
+function abreOuFechaResposta (elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');

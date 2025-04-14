747934907e2bb93f805d9f7ffb2387fc897cb20c
BRAYAN <brayanfranklin88@gmail.com>
2025-04-14 05:26:40 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -6,7 +6,15 @@ document.addEventListener('DOMContentLoaded', function(){
     const alturaHero = heroSection.clientHeight;
 
     window.addEventListener('scroll', function(){
-        console.log(window.scrollY);
+        const posicaoAtual = window.scrollY;
+
+    if (posicaoAtual < alturaHero) {
+        ocultaElementosDoHeader();
+
+    } else {
+        exibeElementosDoHeader();
+    }
+
     })
 
 
@@ -30,6 +38,20 @@ document.addEventListener('DOMContentLoaded', function(){
         questions[i].addEventListener('click',abreOuFechaResposta)
     }
 })
+
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+}
+
+
+
 
 function abreOuFechaResposta (elemento){
     const classe = 'faq__questions__item--is-open';
